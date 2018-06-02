6ca68c424378e01bba9406671efd8d33345e97f8
Main Uddin <mdmain74@gmail.com>
2018-06-03 05:08:50 +0600

    partial commit venue card

--- src/Event/Venues.js
+++ src/Event/Venues.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+import { Card, CardTitle, Col, Container, Row } from 'reactstrap'
 
-class Venues extends Component {
-  render () {
-    return (
-      <div>
-        hello
-      </div>
-    )
-  }
-}
-export default Venues
+const CardStyle = styled(Card)`
+  min-height: 250px;
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  border: none;
+
+  background-image: ${p => `linear-gradient(rgba(20,20,20, .5), rgba(20,20,20, .5)),` + `url(${p['data-bgimg']})`};
+`
+
+const Venues = ({ children, bgImg, size, ...p }) => (
+  <Container>
+    <Row>
+      <Col sm={size || 3} {...p}>
+        <CardStyle body inverse data-bgimg={bgImg} />
+        <CardTitle>{children}</CardTitle>
+      </Col>
+    </Row>
+  </Container>
+)
+
+const VenueCard = styled(Venues)`
+padding-left: 10px;
+padding-right: 10px;
+padding: 20px 10px;
+
+width: 100%;
+background-color: transparent;
+`
+export default VenueCard
--- src/Event/index.js
+++ src/Event/index.js
@@ -4,8 +4,12 @@ import cx from 'classnames'
 import styled from 'styled-components'
 
 import EventForm from './EventForm'
-import Venues from './Venues'
+import VenueCard from './Venues'
 import Footer from '../common/Footer'
+
+import image1 from '../images/hotel.jpg'
+import image2 from '../images/hotel2.jpg'
+import image3 from '../images/hotel3.jpg'
 
 const Root = styled.div.attrs({
   className: p => cx('root', p.className)
@@ -16,7 +20,15 @@ const Root = styled.div.attrs({
 const Event = ({ ui, ...p }) => (
   <Root>
     <EventForm />
-    <Venues />
+    <VenueCard bgImg={image1} size='4'>
+      hello
+    </VenueCard>
+    <VenueCard bgImg={image2} size='4'>
+      hello
+    </VenueCard>
+    <VenueCard bgImg={image3} size='4'>
+      hello
+    </VenueCard>
     <Footer />
   </Root>
 )
