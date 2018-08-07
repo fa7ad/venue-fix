ee28b6cabbcc11690f66e4a8e61edd851e027643
Fahad Hossain <evilinfinite7@gmail.com>
2018-08-07 15:27:18 +0600

    separate out auth logic

--- src/server.js
+++ src/server.js
@@ -6,11 +6,9 @@ import ExpressPouchDB from 'express-pouchdb'
 
 import bcrypt from 'bcrypt'
 import logger from 'morgan'
-import passport from 'passport'
 import bodyParser from 'body-parser'
 import session from 'express-session'
 import PouchSession from 'session-pouchdb-store'
-import { Strategy as LocalStrategy } from 'passport-local'
 
 import React from 'react'
 import App from './App'
@@ -18,15 +16,16 @@ import { StaticRouter } from 'react-router-dom'
 import { renderToString } from 'react-dom/server'
 import { ServerStyleSheet } from 'styled-components'
 
-import User from './server/userModel'
+import auth from './server/auth'
 import renderPage from './server/renderPage'
 
 const PouchDB = Pouch.defaults({
   prefix: path.resolve(process.env.RAZZLE_HOME, 'db', '.db__')
 })
+const usersDB = new PouchDB(process.env.RAZZLE_DB_PREFIX + 'users')
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
 const app = Express()
-
 app.disable('x-powered-by')
 app.use(Express.static(process.env.RAZZLE_PUBLIC_DIR, { index: false }))
 app.use(logger('dev'))
@@ -45,69 +44,7 @@ app.use(
 )
 app.use('/__db', ExpressPouchDB(PouchDB))
 
-const usersDB = new PouchDB(process.env.RAZZLE_DB_PREFIX + 'users')
-
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'phone',
-      passwordField: 'password',
-      session: true
-    },
-    (phone, plain, cb) =>
-      usersDB
-        .get(phone)
-        .then(usr =>
-          cb(null, usr && bcrypt.compareSync(plain, usr.password) ? usr : false)
-        )
-        .catch(cb)
-  )
-)
-
-passport.serializeUser(function (user, cb) {
-  cb(null, user._id)
-})
-
-passport.deserializeUser(function (id, cb) {
-  usersDB.get(id).catch(err => cb(err)).then(user => cb(null, user))
-})
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-app
-  .route('/auth')
-  .post(
-    passport.authenticate('local', {
-      successReturnToOrRedirect: '/admin',
-      failureRedirect: '/auth'
-    })
-  )
-  .get(function (req, res) {
-    res.redirect('/?auth=signin')
-  })
-  .put(function (req, res) {
-    if (!req.body) res.status(400).redirect('/')
-    try {
-      const { phone: _id, password, name, address, admin } = User(req.body)
-      bcrypt
-        .hash(password, 10)
-        .then(hash => ({ _id, password: hash, name, address, admin }))
-        .then(user => usersDB.put(user))
-        .then(_ => {
-          res.json({ success: true })
-        })
-    } catch (e) {
-      res.status(400).json({ success: false })
-    }
-  })
-
-app.get(['/signout', '/logout'], function (req, res) {
-  req.logout()
-  res.redirect('/')
-})
-
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
+auth(app, usersDB)
 
 app.get('/*', (req, res) => {
   const context = {}
---
+++ src/server/auth.js
@@ -0,0 +1,71 @@
+import bcrypt from 'bcrypt'
+
+import passport from 'passport'
+import { Strategy as LocalStrategy } from 'passport-local'
+
+import User from './userModel'
+
+export default function (app, usersDB) {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'phone',
+        passwordField: 'password',
+        session: true
+      },
+      (phone, plain, cb) =>
+        usersDB
+          .get(phone)
+          .then(usr =>
+            cb(
+              null,
+              usr && bcrypt.compareSync(plain, usr.password) ? usr : false
+            )
+          )
+          .catch(cb)
+    )
+  )
+
+  passport.serializeUser(function (user, cb) {
+    cb(null, user._id)
+  })
+
+  passport.deserializeUser(function (id, cb) {
+    usersDB.get(id).catch(err => cb(err)).then(user => cb(null, user))
+  })
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  app
+    .route('/auth')
+    .post(
+      passport.authenticate('local', {
+        successReturnToOrRedirect: '/admin',
+        failureRedirect: '/auth'
+      })
+    )
+    .get(function (req, res) {
+      res.redirect('/?auth=signin')
+    })
+    .put(function (req, res) {
+      if (!req.body) res.status(400).redirect('/')
+      try {
+        const { phone: _id, password, name, address, admin } = User(req.body)
+        bcrypt
+          .hash(password, 10)
+          .then(hash => ({ _id, password: hash, name, address, admin }))
+          .then(user => usersDB.put(user))
+          .then(_ => {
+            res.json({ success: true })
+          })
+      } catch (e) {
+        res.status(400).json({ success: false })
+      }
+    })
+
+  app.get(['/signout', '/logout'], function (req, res) {
+    req.logout()
+    res.redirect('/')
+  })
+}
